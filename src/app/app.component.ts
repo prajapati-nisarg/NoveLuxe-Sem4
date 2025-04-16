e112aa114004e1ff20acfb54909448a0b7c52ef3
prajapati-nisarg <nisargfprajapati@gmail.com>
2025-04-16 18:12:32 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from '../Components/header/header.component';
+import { SidebarComponent } from '../Components/sidebar/sidebar.component';
+import { NavbarComponent } from '../Components/navbar/navbar.component';
+import { FooterComponent } from '../Components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../Components/home/home.component';
+import { GenreComponent } from '../Components/genre/genre.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'genre',
+        component: GenreComponent
+    }
+];
